851e43e4ff1626bfa25b1d06b7ba91183f242e98
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-04-05 15:41:53 -0300

    Functions that change the classes and colors for the themes

--- assets/js/change-theme.js
+++ assets/js/change-theme.js
@@ -1,3 +1,4 @@
+const page = $('main.page');
 const light_theme = $('button.button-light');
 const dark_theme = $('button.button-dark');
 
@@ -5,10 +6,12 @@ light_theme.on('click', function(){
     $(this).fadeOut(100, function(){
         dark_theme.fadeIn(100);
     });
+    page.removeClass('light-theme').addClass('dark-theme');
 });
 
 dark_theme.on('click', function(){
     $(this).fadeOut(100, function(){
         light_theme.fadeIn(100);
     });
+    page.removeClass('dark-theme').addClass('light-theme');
 });
