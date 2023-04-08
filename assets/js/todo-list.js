e5af45d852ba712f05dceb8800e0a4c6316f181a
Melksedeque Silva - GPS <freelancer@melksedeque.com.br>
2023-04-07 21:17:17 -0300

    ToDo List Functions: Complete task; Delete task; Delete completed tasks;

--- assets/js/todo-list.js
+++ assets/js/todo-list.js
@@ -1,62 +1,23 @@
-const main = () => {
-    prepareDOMElements();
-    prepareDOMEvents();
-}
+$(function(){
+    // Create list item
+    $('input#input_new_todo').on('keyup', function (e) {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            alert('Melk');
+        }
+    });
 
-const prepareDOMElements = () => {
-    toDoInput = document.querySelector('input[name="input_new_todo"]');
-    errorInfo = document.querySelector('p.error-info');
-    ulList = document.querySelector('section.list-todo ul.list-items');
-}
+    // Complete task
+    $('button.complete').on('click', function() {
+        $(this).closest('li').addClass('completed');
+    });
 
-const prepareDOMEvents = () => {
-    toDoInput.addEventListener('keyup', enterKeyCheck);
-    ulList.addEventListener('click', checkClick);
-}
+    // Delete list item
+    $('button.delete').on('click', function() {
+        $(this).closest('li').fadeOut('fast');
+    });
 
-const addNewToDo = () => {
-    if (toDoInput.value != ''){
-        newToDo = document.createElement('li');
-        newToDo.textContent = toDoInput.value;
-        //dodajmey do naszego ToDo toolsy (przyciski)
-        // createToolAreal();
-        //dodajemy naszego ToDo do ul listy
-        ulList.append(newToDo);
-        
-        //czyscimy error jak i input po dodaniu ToDo
-        toDoInput.value = '';
-        errorInfo.textContent = '';
-    } else {
-        errorInfo.classList.toggle('alert-danger');
-        errorInfo.textContent = 'Você não pode adicionar um item vazio!';
-    }
-}
-
-//wykonujemy też dodanie todo na enterze
-const enterKeyCheck = (e) => {
-    if(e.key == 'Enter'){
-        addNewToDo();
-    }
-}
-
-//funkcjs sprawdzająca w co klikamy (aby wiedziec czy zakonczyc task czy moze go usunac czy edytowac)
-const checkClick = (e) => {
-    if(e.target.matches('.complete')){
-        e.target.closest('li').classList.toggle('completed'); //po nacisnieciu complete do najblizszego elementu li (dziadka) dodajemy klase completed
-        e.target.classList.toggle('completed');
-
-    } else if (e.target.matches('.delete')) { //else if poniewaz gdybysmy klikneli wszedzie poza delete tez by sie wykonal warunek
-        deleteToDo(e);
-    }
-}
-
-const deleteToDo = (e) => {
-    e.target.closest('li').remove();
-
-    const allToDos = ulList.querySelectorAll('li');
-    if (allToDos.length == 0) {
-        errorInfo.textContent = 'Brak zadań na liście.'
-    }
-}
-
-document.addEventListener('DOMContentLoaded', main);
+    // Delete all completed tasks
+    $('button.clear-items').on('click', function() {
+        $('main.page').find('li.completed').fadeOut('fast');
+    });
+})
