e558b5ae8ce4597727ae4e21322eedac745d2a7c
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-06-24 16:24:35 -0300

    Save state in localStorage after drop the item

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,44 +237,44 @@ function saveData(){
 
 function dragAndDrop() {
     let items = document.querySelectorAll('#task-list li'),
-        dragged = null;
+        dragged = null
 
     for(let i of items) {
         i.addEventListener("dragstart", function() {
-            dragged = this;
+            dragged = this
 
             for(let it of items) {
                 if(it != dragged) {
-                    it.classList.add('hint');
+                    it.classList.add('hint')
                 }
             }
         });
 
         i.addEventListener("dragenter", function() {
             if(this != dragged) {
-                this.classList.add('active');
+                this.classList.add('active')
             }
         });
 
         i.addEventListener("dragleave", function() {
-            this.classList.remove('active');
+            this.classList.remove('active')
         });
         
         i.addEventListener("dragend", function() {
             for(let it of items) {
                 if(it != dragged) {
-                    it.classList.remove('hint');
-                    it.classList.remove('active');
+                    it.classList.remove('hint')
+                    it.classList.remove('active')
                 }
             }
         });
 
         i.addEventListener("dragover", function(e) {
-            e.preventDefault();
+            e.preventDefault()
         });
 
         i.addEventListener("drop", function(e) {
-            e.preventDefault();
+            e.preventDefault()
 
             if(this != dragged) {
                 let all = document.querySelectorAll('#task-list li'),
@@ -282,20 +282,20 @@ function dragAndDrop() {
                     droppedpos = 0;
                 
                 for(let it = 0; it < all.length; it++) {
-                    if(dragged == all[it]) { draggedpos = it; }
-                    if(this == all[it]) { droppedpos = it; }
+                    if(dragged == all[it]) { draggedpos = it }
+                    if(this == all[it]) { droppedpos = it }
                 }
 
                 if(draggedpos < droppedpos) {
-                    this.parentNode.insertBefore(dragged, this.nextSibling);
+                    this.parentNode.insertBefore(dragged, this.nextSibling)
                 }
                 else {
-                    this.parentNode.insertBefore(dragged, this);
+                    this.parentNode.insertBefore(dragged, this)
                 }
             }
-            saveData()
-        });
+        })
     }
+    saveData()
 }
 
 showTasks()
